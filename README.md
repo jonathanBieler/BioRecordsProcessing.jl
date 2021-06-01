a15856785875a2793e825f9cbd976419624305e3
Jonathan Bieler <jonathan.bieler@alumni.epfl.ch>
2021-06-01 16:44:48 +0200

    initial commit

--- README.md
+++ README.md
@@ -4,3 +4,54 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://jonathanBieler.github.io/BioRecordsProcessing.jl/dev)
 [![Build Status](https://travis-ci.com/jonathanBieler/BioRecordsProcessing.jl.svg?branch=master)](https://travis-ci.com/jonathanBieler/BioRecordsProcessing.jl)
 [![Coverage](https://codecov.io/gh/jonathanBieler/BioRecordsProcessing.jl/branch/master/graph/badge.svg)](https://codecov.io/gh/jonathanBieler/BioRecordsProcessing.jl)
+
+Easily process files containing biological records. If several files are found in a directory
+they will be processed in parallel (one thread per file).
+
+## Examples
+
+### Filter fasta reads shorter than 50bp :
+
+```julia
+using BioRecordsProcessing, FASTX
+
+#test only on first 100 records
+BioRecordsProcessing.process_directory(FASTX.FASTA, input_directory, "*.fa", output_directory; max_records=100) do record
+    return FASTX.FASTA.seqlen(record) < 50 ? nothing : record
+end
+```
+
+### Trim 10bp at the start of paired-end reads :
+
+```julia
+using BioRecordsProcessing, FASTX
+
+get_f2 = f1 -> replace(f1, "_1" => "_2")#function to get the name of the file containing second reads
+
+trim_record(r, trim) =  begin 
+    s = FASTQ.sequence(r)
+    sel = min(length(s), trim):length(s)
+    return FASTQ.Record(FASTQ.identifier(r), FASTQ.description(r), s[sel], FASTQ.quality(r)[sel])
+end
+
+BioRecordsProcessing.process_directory_paired(FASTX.FASTQ, input_directory, "*_1.fastq", get_f2, output_directory) do r1,r2
+    r1, r2 = trim_record(r1, 10), trim_record(r2, 10)
+    return r1, r2
+end
+```
+
+### Read a BAM file and groups reads by template name
+
+```julia
+using BioRecordsProcessing, XAM
+
+grouper = BioRecordsProcessing.RecordGrouper{XAM.BAM.Record, String}(
+    r -> XAM.BAM.tempname(r),           # key used to group reads 
+    records -> length(records) == 2     # a group is released when this condition is met
+)
+
+BioRecordsProcessing.process(XAM.BAM, grouper, bam_file, output_directory) do r1,r2
+    # ... 
+    return (r1, r2)
+end
+```
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,67 @@
 using BioRecordsProcessing
-using Test
+using Test, FASTX, XAM, BioSequences, FormatSpecimens
 
 @testset "BioRecordsProcessing.jl" begin
-    # Write your tests here.
+    
+    @testset "FASTA" begin
+        mktempdir() do dir
+            
+            filepath = joinpath(dir, "test.fa")
+            writer = open(FASTA.Writer, filepath)
+            for i=1:10
+                write(writer, FASTA.Record("seq1", randdnaseq(i)))
+            end
+            close(writer)
+
+            N_larger_than_5 = 0
+            BioRecordsProcessing.process_directory(FASTX.FASTA, dir, "*.fa", dir; prefix = "out") do record
+                N_larger_than_5 += FASTX.FASTA.seqlen(record) > 5
+                return record
+            end
+            @test N_larger_than_5 == 5
+
+        end
+    end
+
+    @testset "Paired FASTA" begin
+        mktempdir() do dir
+
+            indir = path_of_format("FASTA")
+            f2 = f1 -> replace(f1, "_1" => "_2")
+            
+            BioRecordsProcessing.process_directory_paired(FASTX.FASTA, indir, "multi_1.fasta", f2, dir; prefix = "out") do r1, r2
+                return r1, r2
+            end
+        end
+    end
+
+    @testset "BAM" begin
+        mktempdir() do dir
+            spec = list_valid_specimens("BAM")
+            bam = joinpath(path_of_format("BAM"), "R_12h_D06.uniq.q40.bam" )
+
+            BioRecordsProcessing.process(XAM.BAM, bam, dir; prefix = "out") do record
+                return record
+            end
+        end
+    end
+
+    @testset "Paired BAM" begin
+        mktempdir() do dir
+            rg = BioRecordsProcessing.RecordGrouper{XAM.BAM.Record, String}(
+                XAM.BAM.tempname, 
+                records -> length(records) == 2
+            )
+            spec = list_valid_specimens("BAM")
+            bam = joinpath(path_of_format("BAM"), "R_12h_D06.uniq.q40.bam" )
+
+            BioRecordsProcessing.process(XAM.BAM, rg, bam, dir; prefix = "out") do r1,r2
+                (r1, r2)
+            end
+        end
+    end
+    
+
 end
+
+
