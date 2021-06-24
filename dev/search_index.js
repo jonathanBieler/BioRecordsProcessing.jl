9e1e6c2756acdb332be8c6fc0d4b48d179d04fd0
Documenter.jl <documenter@juliadocs.github.io>
2021-06-24 13:21:00 +0000

    build based on a4ec97e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BioRecordsProcessing","category":"page"},{"location":"#BioRecordsProcessing","page":"Home","title":"BioRecordsProcessing","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BioRecordsProcessing.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BioRecordsProcessing]","category":"page"}]
+}
